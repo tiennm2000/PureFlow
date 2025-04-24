1b037effe42d5f06c4371924e3b4ac77ab485704
Nguyễn Tiểu Bảo <baonguyen241201@gmail.com>
2025-04-24 11:22:58 +0700

    create module accounts
    create api login, register, logout
    gen refresh, access token when login
    and add token on blacklist when logout

---
+++ Django_api/accounts/controller.py
@@ -0,0 +1,45 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated 
+from .serializer import UserRegisterSerializer, LoginSerializer
+from .service import AccountService
+
+
+class RegisterAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = AccountService.register(
+            username=serializer.validated_data['username'],
+            email=serializer.validated_data['email'],
+            password=serializer.validated_data['password'],
+            phone=serializer.validated_data['phone']
+        )
+        return Response({'detail': 'Đăng ký thành công.'}, status=status.HTTP_201_CREATED)
+
+class LoginAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        tokens = AccountService.login(
+            username=serializer.validated_data['username'],
+            password=serializer.validated_data['password']
+        )
+        if not tokens:
+            return Response({'detail': 'Invalid credentials.'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(tokens, status=status.HTTP_200_OK)
+
+class LogoutAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        refresh_token = request.data.get('refresh')
+        if not refresh_token:
+            return Response({'detail': 'Refresh token required.'}, status=status.HTTP_400_BAD_REQUEST)
+        AccountService.logout(refresh_token)
+        return Response({'detail': 'Đăng xuất thành công'}, status=status.HTTP_204_NO_CONTENT)
