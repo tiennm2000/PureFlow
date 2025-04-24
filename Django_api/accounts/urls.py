b3bd5bc6d41cfe92472aee1c50b11e11c4201f48
Nguyễn Tiểu Bảo <baonguyen241201@gmail.com>
2025-04-24 11:51:36 +0700

    delete
    delete

--- Django_api/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .controller import RegisterAPIView,LoginAPIView, LogoutAPIView
-
-
-urlpatterns = [
-    path('register/', RegisterAPIView.as_view(), name='register'),
-    path('login/', LoginAPIView.as_view(), name='login'),
-    path('logout/', LogoutAPIView.as_view(), name='logout'),
-]
