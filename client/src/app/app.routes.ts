d74113dbdd562d1a6423d0a5b4054afe53dd8642
tiennmxemmex <tiennm.xemmex@gmail.com>
2025-04-25 13:04:48 +0700

    auth routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./pages/auth/auth.module').then((m) => m.AuthModule),
+  },
+];
